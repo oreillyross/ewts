d7f79f50708d87ff9ab8b4e30d57729f23ad563e
Ross O'Reilly <oreillyross@gmail.com>
2018-09-10 11:47:33 +0000

    add docs

---
+++ ewts/src/app/forms/EventForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class EventForm extends React.Component {
+    render() {
+        return (
+          <div> event form    </div>
+        )
+    }
+}
+export default EventForm
