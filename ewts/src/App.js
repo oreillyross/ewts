8e71599643b3944c50f94629e01d0c1154110800
Ross O'Reilly <oreillyross@gmail.com>
2018-09-11 09:58:53 +0000

    day 2

--- ewts/src/App.js
+++ ewts/src/App.js
@@ -8,8 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Early Warning Threat System</h1>
         </header>
         <p className="App-intro">
           <EventForm />
--- ewts/src/app/forms/EventForm.js
+++ ewts/src/app/forms/EventForm.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import  styled  from 'styled-components'
+
+
+const Title = styled.div`
+  margin: 3px;
+  color: #585858;
+  border: 1px solid grey;
+  padding: 4px;
+  padding-left: 20px;
+  text-align: left;
+  font-family: 'Roboto Condensed', sans-serif;
+`
+console.log(process.env.GRAPHCOOL_URL)
 
 class EventForm extends React.Component {
     render() {
         return (
-          <div> event form    </div>
+          <Title> Create a new event </Title>
+          
         )
     }
 }
