817ec2b7724ab8330870be86a0bd04de45db7418
Ross O'Reilly <oreillyross@gmail.com>
2018-11-05 12:56:37 +0000

    add react virtualized to events table

--- ewts/src/app/views/Events.js
+++ ewts/src/app/views/Events.js
@@ -1,31 +1,52 @@
-import React from "react";
-import EventTable from "./EventTable";
+import React, { useEffect, useState } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import { Column, Table } from "react-virtualized";
+import styles from "./Events.module.css";
 
-class Events extends React.Component {
-  constructor() {
-    super();
-    this.state = { sources: [] };
+const _rowClassName = ({ index }) => {
+  if (index < 0) {
+    return styles.headerRow;
+  } else {
+    return index % 2 === 0 ? styles.evenRow : styles.oddRow;
   }
+};
 
-  componentDidMount() {
-    fetch("https://pewts-server-oreillyross.c9users.io/roo", { mode: "cors" })
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        this.setState((prevState, props) => ({ sources: data }));
-      });
+const EVENTSQUERY = gql`
+  {
+    allEvents {
+      id
+      title
+      description
+      source
+      href
+      unread
+    }
   }
+`;
 
-  render() {
-    return (
-      <div>
-        {" "}
-        <h1> Events </h1>
-        <EventTable sources={this.state.sources} />
-      </div>
-    );
-  }
-}
+const Events = () => (
+  <Query query={EVENTSQUERY}>
+    {({ loading, error, data }) => {
+      if (loading) return "Loading data.......";
+      if (error) return `Error: ${error.message}`;
+      const titles = data.allEvents.map(event => event.title);
+      return (
+        <Table
+          width={1400}
+          height={500}
+          headerHeight={40}
+          rowHeight={30}
+          rowCount={data.allEvents.length}
+          rowGetter={({ index }) => data.allEvents[index]}
+          rowClassName={_rowClassName}
+          headerClassName="headers"
+        >
+          <Column label="Title" dataKey="title" width={800} className="rows" />
+        </Table>
+      );
+    }}
+  </Query>
+);
 
 export default Events;
--- ewts/src/data/apolloClient.js
+++ ewts/src/data/apolloClient.js
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_DEV_GRAPHCOOL_URL
+  uri: process.env.REACT_APP_DEV_PEWTS_URL
 });
 
 export default client;
