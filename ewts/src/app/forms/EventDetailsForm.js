033ef557dda6595d187c62b79b4702dc58e5074a
Ross O'Reilly <oreillyross@gmail.com>
2018-11-26 14:46:09 +0000

    update events working, all hail the graphql gods

--- ewts/src/app/forms/EventDetailsForm.js
+++ ewts/src/app/forms/EventDetailsForm.js
@@ -16,23 +16,46 @@ import moment from "moment";
 import { Formik, Field, Form, FieldArray } from "formik";
 import Chip from "@material-ui/core/Chip";
 
-const EVENTMUTATION = gql`
-  mutation updateEvent(
+const UPDATE_EVENT = gql`
+  mutation UpdateEvent(
     $id: ID!
-    $title: String
+    $title: String!
     $description: String
-    $eventDate: Float
-    $crawlDate: Float
+    $eventDate: DateTime
+    $crawlDate: DateTime
+    $source: String
+    $href: String
   ) {
-    updateEvent(
-      id: $id
-      title: $title
-      description: $description
-      crawlDate: $crawlDate
+    eventUpdater: updateEvent(
+      data: {
+        title: $title
+        description: $description
+        eventDate: $eventDate
+        crawlDate: $crawlDate
+        source: $source
+        href: $href
+      }
+      where: { id: $id }
     ) {
       id
       title
       description
+    }
+  }
+`;
+
+const CONNECT_DESCRIPTOR = gql`
+  mutation connectDescriptor($id: ID!, $tag: String) {
+    connectDescriptor: updateEvent(
+      where: { id: $id }
+      data: { descriptors: { connect: { tag: $tag } } }
+    ) {
+      id
+      title
+      descriptors {
+        id
+        tag
+      }
     }
   }
 `;
@@ -57,174 +80,203 @@ const REMOVE_DESCRIPTOR = gql`
 
 const EventDetailsForm = ({ event }) => {
   const {
+    id,
     title,
     description,
     source,
     href,
     eventDate,
     crawlDate,
-    tags,
     descriptors
   } = event;
-  console.log(descriptors);
   return (
-    <Formik
-      initialValues={{
-        title,
-        description,
-        source,
-        href,
-        eventDate:
-          moment(eventDate).format("YYYY-MM-DD") || moment.format("YYYY-MM-DD"),
-        crawlDate:
-          moment(crawlDate).format("YYYY-MM-DD") || moment.format("YYYY-MM-DD"),
-        descriptors: descriptors.map(d => d.tag)
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          setSubmitting(false);
-          alert(JSON.stringify(values, null, 2));
-        }, 500);
-      }}
-    >
-      {({ values, touched, errors, dirty, isSubmitting, handleChange }) => {
-        return (
-          <Form>
-            <div className={styles.box}>
-              <div className={styles.header}>Event Form</div>
+    <Mutation mutation={CONNECT_DESCRIPTOR}>
+      {(connectDescriptor, { data }) => (
+        <Mutation mutation={UPDATE_EVENT}>
+          {(eventUpdater, { data }) => (
+            <Formik
+              initialValues={{
+                title,
+                description,
+                source,
+                href,
+                eventDate:
+                  moment(eventDate).format("YYYY-MM-DD") ||
+                  moment.format("YYYY-MM-DD"),
+                crawlDate:
+                  moment(crawlDate).format("YYYY-MM-DD") ||
+                  moment.format("YYYY-MM-DD"),
+                descriptors: descriptors.map(d => d.tag)
+              }}
+              onSubmit={(values, { setSubmitting }) => {
+                const { descriptors } = values;
+                if (descriptors.length > 0) {
+                  descriptors.map(d =>
+                    connectDescriptor({ variables: { id: id, tag: d } })
+                  );
+                }
 
-              <div className={styles.oneliner}>
-                <Field
-                  name="title"
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      id="title"
-                      fullWidth
-                      label="Title of the event"
-                      type="text"
-                    />
-                  )}
-                />
-              </div>
+                eventUpdater({ variables: { id, ...values } });
 
-              <div className={styles.oneliner}>
-                <Field
-                  name="description"
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      id="description"
-                      multiline
-                      rows={8}
-                      fullWidth
-                      label="Description of the event"
-                      type="text"
-                    />
-                  )}
-                />
-              </div>
+                setTimeout(() => {
+                  setSubmitting(false);
+                  alert(JSON.stringify(values, null, 2));
+                }, 500);
+              }}
+            >
+              {({
+                values,
+                touched,
+                errors,
+                dirty,
+                isSubmitting,
+                handleChange
+              }) => {
+                return (
+                  <Form>
+                    <div className={styles.box}>
+                      <div className={styles.header}>Event Form</div>
 
-              <div className={styles.left}>
-                <Field
-                  name="source"
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      id="source"
-                      fullWidth
-                      label="Source of article"
-                      type="text"
-                    />
-                  )}
-                />
-              </div>
-
-              <div className={styles.right}>
-                <Field
-                  name="href"
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      id="href"
-                      fullWidth
-                      label="Hyperlink of the article"
-                      type="url"
-                    />
-                  )}
-                />
-              </div>
-
-              <div className={styles.left}>
-                <Field
-                  name="eventDate"
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      id="eventDate"
-                      locale="en-gb"
-                      fullWidth
-                      label="Date of the event"
-                      type="date"
-                    />
-                  )}
-                />
-              </div>
-
-              <div className={styles.right}>
-                <Field
-                  name="crawlDate"
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      id="crawlDate"
-                      fullWidth
-                      label="Date article was sourced"
-                      type="date"
-                    />
-                  )}
-                />
-              </div>
-
-              <div className={styles.oneliner}>
-                <FieldArray
-                  name="descriptors"
-                  render={arrayHelpers => (
-                    <Mutation mutation={REMOVE_DESCRIPTOR}>
-                      {(removeDescriptor, { data }) => (
-                        <Mutation mutation={ADD_DESCRIPTOR}>
-                          {(createDescriptor, { data }) => (
-                            <ChipInput
+                      <div className={styles.oneliner}>
+                        <Field
+                          name="title"
+                          render={({ field }) => (
+                            <TextField
+                              {...field}
+                              id="title"
                               fullWidth
-                              label="Descriptors"
-                              value={values.descriptors}
-                              onAdd={e => {
-                                createDescriptor({ variables: { tag: e } });
-                                arrayHelpers.push(e);
-                              }}
-                              onDelete={e => {
-                                removeDescriptor({ variables: { tag: e } });
-                                arrayHelpers.remove(
-                                  values.descriptors.indexOf(e)
-                                );
-                              }}
+                              label="Title of the event"
+                              type="text"
                             />
                           )}
-                        </Mutation>
-                      )}
-                    </Mutation>
-                  )}
-                />
-              </div>
+                        />
+                      </div>
 
-              <div className={styles.oneliner}>
-                <button type="submit"> Update</button>
-              </div>
-            </div>{" "}
-          </Form>
-        );
-      }}
-    </Formik>
+                      <div className={styles.oneliner}>
+                        <Field
+                          name="description"
+                          render={({ field }) => (
+                            <TextField
+                              {...field}
+                              id="description"
+                              multiline
+                              rows={8}
+                              fullWidth
+                              label="Description of the event"
+                              type="text"
+                            />
+                          )}
+                        />
+                      </div>
+
+                      <div className={styles.left}>
+                        <Field
+                          name="source"
+                          render={({ field }) => (
+                            <TextField
+                              {...field}
+                              id="source"
+                              fullWidth
+                              label="Source of article"
+                              type="text"
+                            />
+                          )}
+                        />
+                      </div>
+
+                      <div className={styles.right}>
+                        <Field
+                          name="href"
+                          render={({ field }) => (
+                            <TextField
+                              {...field}
+                              id="href"
+                              fullWidth
+                              label="Hyperlink of the article"
+                              type="url"
+                            />
+                          )}
+                        />
+                      </div>
+
+                      <div className={styles.left}>
+                        <Field
+                          name="eventDate"
+                          render={({ field }) => (
+                            <TextField
+                              {...field}
+                              id="eventDate"
+                              locale="en-gb"
+                              fullWidth
+                              label="Date of the event"
+                              type="date"
+                            />
+                          )}
+                        />
+                      </div>
+
+                      <div className={styles.right}>
+                        <Field
+                          name="crawlDate"
+                          render={({ field }) => (
+                            <TextField
+                              {...field}
+                              id="crawlDate"
+                              fullWidth
+                              label="Date article was sourced"
+                              type="date"
+                            />
+                          )}
+                        />
+                      </div>
+
+                      <div className={styles.oneliner}>
+                        <FieldArray
+                          name="descriptors"
+                          render={arrayHelpers => (
+                            <Mutation mutation={REMOVE_DESCRIPTOR}>
+                              {(removeDescriptor, { data }) => (
+                                <Mutation mutation={ADD_DESCRIPTOR}>
+                                  {(createDescriptor, { data }) => (
+                                    <ChipInput
+                                      fullWidth
+                                      label="Descriptors"
+                                      value={values.descriptors}
+                                      onAdd={e => {
+                                        createDescriptor({
+                                          variables: { tag: e }
+                                        });
+                                        arrayHelpers.push(e);
+                                      }}
+                                      onDelete={e => {
+                                        removeDescriptor({
+                                          variables: { tag: e }
+                                        });
+                                        arrayHelpers.remove(
+                                          values.descriptors.indexOf(e)
+                                        );
+                                      }}
+                                    />
+                                  )}
+                                </Mutation>
+                              )}
+                            </Mutation>
+                          )}
+                        />
+                      </div>
+
+                      <div className={styles.oneliner}>
+                        <button type="submit"> Update</button>
+                      </div>
+                    </div>{" "}
+                  </Form>
+                );
+              }}
+            </Formik>
+          )}
+        </Mutation>
+      )}
+    </Mutation>
   );
 };
 
