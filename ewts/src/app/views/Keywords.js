b48dca60f9eae31df990702e04df53bcfe4c33f8
Ross O'Reilly <oreillyross@gmail.com>
2018-12-17 16:16:22 +0000

    keywords edit box skeleton added

--- ewts/src/app/views/Keywords.js
+++ ewts/src/app/views/Keywords.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import gql from "graphql-tag";
 import { Query, Mutation } from "react-apollo";
 import Paper from "@material-ui/core/Paper";
@@ -6,6 +6,7 @@ import { Column, Table, AutoSizer } from "react-virtualized";
 import { Formik } from "formik";
 import Input from "@material-ui/core/Input";
 import Button from "@material-ui/core/Button";
+import styled from "styled-components";
 
 const KEYWORDS_QUERY = gql`
   query Keywords {
@@ -18,10 +19,47 @@ const KEYWORDS_QUERY = gql`
 
 const ADD_KEYWORD = gql`
   mutation createKeyword($searchterm: String!) {
-    createKeyword(searchterm: $searchterm) {
+    createKeyword(data: { searchterm: $searchterm }) {
       id
       searchterm
     }
+  }
+`;
+
+const EditKeyword = ({ id, searchterm }) => {
+  console.log(id);
+  return <div>{searchterm}</div>;
+};
+
+const KeywordRow = ({ id, searchterm }) => {
+  const [editing, updateEdit] = useState(false);
+
+  console.log(editing);
+  if (!editing) {
+    return (
+      <StyledKeywordRow
+        onClick={e => {
+          updateEdit(!editing);
+        }}
+      >
+        {searchterm}
+      </StyledKeywordRow>
+    );
+  } else {
+    return (
+      <EditKeyword
+        id={id}
+        searchterm={searchterm}
+        onClick={e => updateEdit(!editing)}
+      />
+    );
+  }
+};
+const StyledKeywordRow = styled.div`
+  font-size: 1.2em;
+  background-color: #e9ece5;
+  &:hover {
+    background-color: #b3c2bf;
   }
 `;
 
@@ -54,7 +92,10 @@ const Keywords = () => {
                         className="rows"
                         cellRenderer={({ rowData }) => {
                           return (
-                            <a href={rowData.href}>{rowData.searchterm}</a>
+                            <KeywordRow
+                              id={rowData.id}
+                              searchterm={rowData.searchterm}
+                            />
                           );
                         }}
                       />
@@ -63,14 +104,25 @@ const Keywords = () => {
                 )}
               </AutoSizer>
             </Paper>
-            <Mutation mutation={ADD_KEYWORD}>
-              {(createKeyword, { data }) => {
+            <Mutation
+              mutation={ADD_KEYWORD}
+              ignoreResults
+              update={(cache, { data: { createKeyword } }) => {
+                const { keywords } = cache.readQuery({ query: KEYWORDS_QUERY });
+                cache.writeQuery({
+                  query: KEYWORDS_QUERY,
+                  data: { keywords: keywords.concat([createKeyword]) }
+                });
+              }}
+            >
+              {createKeyword => {
                 return (
                   <Formik
                     initialValues={{ searchterm: "" }}
                     onSubmit={(values, { setSubmitting }) => {
-                      createKeyword(data);
-                      console.log(values);
+                      createKeyword({
+                        variables: { searchterm: values.searchterm }
+                      });
                       values.searchterm = "";
                       setSubmitting(false);
                     }}
